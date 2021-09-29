7b16c3644cd26462218b33f883a2fb0dca45b562
Adria Ivanitsky <littleivanitsky@gmail.com>
2021-09-29 15:52:23 -0700

    added images and sounds with javascript to animal farm

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 // import functions and grab DOM elements
-
+const catImg = document.getElementById('cat');
+const catSound = document.getElementById('catsound');
+const pugImg = document.getElementById('pug');
+const dogSound = document.getElementById('dogsound');
+const horseImg = document.getElementById('horse');
+const horseSound = document.getElementById('horsesound');
 // initialize global state
 
 // set event listeners 
+
+catImg.addEventListener('click', ()=>{
+    catSound.play();
+});
+pugImg.addEventListener('click', ()=>{
+    dogSound.play();
+});
+horseImg.addEventListener('click', ()=>{
+    horseSound.play();
+});
+
+
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
